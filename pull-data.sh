81170e2e2779b26aaae3766533af4edfe8ca24d4
Cj Malone <CjMalone@mail.com>
2021-02-25 11:13:45 +0000

    Add geojson output

--- pull-data.sh
+++ pull-data.sh
@@ -7,10 +7,11 @@ mkdir -p "data/"
 for file in ./Queries/*.ql; do
 	QUERY_NAME="${file:10:-3}"
 	QUERY=$(cat "$file")
-	echo $QUERY
+	echo "$QUERY_NAME"
 
 	curl -G --data-urlencode "data=$QUERY" -o "data/$QUERY_NAME.osm" $OVERPASS_API
-	# ogr2ogr data/$QUERY_NAME.geojson data/$QUERY_NAME.osm
+	ogr2ogr -skipfailures --config OSM_CONFIG_FILE "Queries/$QUERY_NAME.conf.ini" "data/$QUERY_NAME.points.geojson" "data/$QUERY_NAME.osm" points
+	ogr2ogr -skipfailures --config OSM_CONFIG_FILE "Queries/$QUERY_NAME.conf.ini" "data/$QUERY_NAME.multipolygons.geojson" "data/$QUERY_NAME.osm" multipolygons
 	# ogr2ogr data/$QUERY_NAME.shp data/$QUERY_NAME.osm
 done
 
