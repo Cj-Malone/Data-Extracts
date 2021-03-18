c4dd4c63d0bcbdcf24e8a7a0a05aa9c0271c9bfe
Cj Malone <CjMalone@mail.com>
2021-03-18 22:52:09 +0000

    Prefix output with date

--- pull-data.sh
+++ pull-data.sh
@@ -2,16 +2,18 @@
 
 OVERPASS_API="https://overpass-api.de/api/interpreter"
 
-mkdir -p "data/"
+mkdir "Data"
+PREFIX=$(date +"%Y-%m-%d")
 
 for file in ./Queries/*.ql; do
 	QUERY_NAME="${file:10:-3}"
 	QUERY=$(cat "$file")
 	echo "$QUERY_NAME"
 
-	curl -G --data-urlencode "data=$QUERY" -o "data/$QUERY_NAME.osm" $OVERPASS_API
-	ogr2ogr -skipfailures --config OSM_CONFIG_FILE "Queries/$QUERY_NAME.conf.ini" "data/$QUERY_NAME.points.geojson" "data/$QUERY_NAME.osm" points
-	ogr2ogr -skipfailures --config OSM_CONFIG_FILE "Queries/$QUERY_NAME.conf.ini" "data/$QUERY_NAME.multipolygons.geojson" "data/$QUERY_NAME.osm" multipolygons
-	# ogr2ogr data/$QUERY_NAME.shp data/$QUERY_NAME.osm
+	curl -G --data-urlencode "data=$QUERY" -o "Data/$QUERY_NAME.osm" $OVERPASS_API
+	ogr2ogr -skipfailures --config OSM_CONFIG_FILE "Queries/$QUERY_NAME.conf.ini" "Data/$PREFIX-$QUERY_NAME.points.geojson" "Data/$QUERY_NAME.osm" points
+	ogr2ogr -skipfailures --config OSM_CONFIG_FILE "Queries/$QUERY_NAME.conf.ini" "Data/$PREFIX-$QUERY_NAME.multipolygons.geojson" "Data/$QUERY_NAME.osm" multipolygons
+	# ogr2ogr -skipfailures --config OSM_CONFIG_FILE "Queries/$QUERY_NAME.conf.ini" "Data/$PREFIX-$QUERY_NAME.points.shp" "Data/$QUERY_NAME.osm" points
+	# ogr2ogr -skipfailures --config OSM_CONFIG_FILE "Queries/$QUERY_NAME.conf.ini" "Data/$PREFIX-$QUERY_NAME.multipolygons.shp" "Data/$QUERY_NAME.osm" multipolygons
 done
 
